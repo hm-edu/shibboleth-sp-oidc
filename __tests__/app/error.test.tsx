b4d40624795c02a196293c93236fbca3128b9336
Michael Fuchs <development@michifuchs.com>
2024-08-09 13:58:37 +0200

    feat: use error and 404 images from ci support (#158)

--- __tests__/app/error.test.tsx
+++ __tests__/app/error.test.tsx
@@ -2,24 +2,17 @@ import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/react';
 import Error from '@/app/error';
 
-const error = {
-  error: {
-    name: 'Error',
-    message: 'Test error message',
-  },
-};
-
 describe('Error', () => {
   beforeEach(() => {
-    render(Error(error));
+    render(Error());
   });
 
   it('should exists', () => {
-    expect(screen.getByRole('alert')).toBeDefined();
+    expect(screen.findByTestId('error-illustration')).toBeDefined();
   });
 
   it('should match snapshot', () => {
-    expect(screen.getByRole('alert')).toMatchSnapshot();
+    expect(screen.findByTestId('error-illustration')).toMatchSnapshot();
   });
 
   afterEach(() => {
